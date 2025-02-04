e81af54c54da58bc3b0e757d81bf1c5f1b51b14d
Jorge Sáez <jorgeesg82@gmail.com>
2025-02-04 19:15:01 +0000

    Fix main

--- Core/Src/main.cpp
+++ Core/Src/main.cpp
@@ -7,16 +7,17 @@
 #include "ST-LIB.hpp"
 #include "HVSCUComms.hpp"
 #include "StateMachineHVSCU.hpp"
-
+using namespace HVSCU;
 int main(void) {
 #ifdef SIM_ON
     SharedMemory::start("shm_gpio_HVSCU", "shm_sm_HVSCU");
 #endif
-    STLIB::start(HVSCUComms::HVSCU_IP);
-    StateMachineHVSCU* sm = new StateMachineHVSCU();
+    STLIB::start(Comms::HVSCU_IP);
+    SM* sm = new SM();
 
     while (1) {
         STLIB::update();
+        sm->update();
     }
 }
 
