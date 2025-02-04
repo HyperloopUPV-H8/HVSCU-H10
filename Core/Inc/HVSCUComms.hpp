3dbdfd4d56aafdda53865da6c8b4fb3f0b1f22f5
Jorge Sáez <jorgeesg82@gmail.com>
2025-02-04 19:14:02 +0000

    Initial HVSCU

--- Core/Inc/HVSCUComms.hpp
+++ Core/Inc/HVSCUComms.hpp
@@ -1,12 +1,23 @@
 #pragma once
 
 #include "ST-LIB.hpp"
+namespace HVSCU {
+namespace Comms {
+const uint16_t CS_PORT = 50500;
+const std::string HVSCU_IP = "192.168.1.9";
 
-namespace HVSCUComms {
-    const uint16_t CS_PORT = 50500;
-    const std::string HVSCU_IP = "192.168.1.9";
+ServerSocket* control_station;
+bool open_contactors_order_received, close_contactors_order_received = false;
+HeapOrder* open_contactors_order = new HeapOrder(
+    Orders::CLOSE_CONTACTORS, []() { open_contactors_order_received = true; });
+HeapOrder* close_contactors_order = new HeapOrder(
+    Orders::CLOSE_CONTACTORS, []() { close_contactors_order_received = true; });
 
-    ServerSocket* control_station;
+void start() { control_station = new ServerSocket(IPV4(HVSCU_IP), CS_PORT); };
 
-    void start();
-};
+namespace Orders {
+static constexpr uint16_t CLOSE_CONTACTORS = 900;
+static constexpr uint16_t OPEN_CONTACTORS = 901;
+};  // namespace Orders
+};  // namespace Comms
+};  // namespace HVSCU
--- Core/Inc/StateMachineHVSCU.hpp
+++ Core/Inc/StateMachineHVSCU.hpp
@@ -1,15 +1,21 @@
 #pragma once
 
 #include "ST-LIB.hpp"
+namespace HVSCU {
+enum State { CONNECTING = 0, OPERATIONAL = 1, FAULT = 2 };
 
-enum StateHVSCU { CONNECTING = 0, OPERATIONAL = 1, FAULT = 2 };
-
-class StateMachineHVSCU {
+class SM {
    private:
-    static StateMachine* hvscu_sm;
+    static StateMachine* sm;
+    uint8_t timeout_id;
+
     void add_states();
     void add_transitions();
+    void check_open_contactors_order();
+    void check_close_contactors_order();
 
    public:
-    StateMachineHVSCU();
+    SM();
+    void update();
 };
+};  // namespace HVSCU
