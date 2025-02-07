5dd99b564d94ea4f502e263d7bd0ab859a4de20e
Jorge Sáez <jorgeesg82@gmail.com>
2025-02-07 22:48:57 +0100

    Refactor

---
+++ Core/Src/HVSCU/Contactor.cpp
@@ -0,0 +1,20 @@
+#include "HVSCU/Contactor.hpp"
+
+Contactor::Contactor(DigitalOutput output, bool normally_opened)
+    : output(output), normally_opened(normally_opened) {}
+
+void Contactor::open() {
+    if (normally_opened)
+        output.turn_off();
+    else {
+        output.turn_on();
+    }
+}
+
+void Contactor::close() {
+    if (normally_opened)
+        output.turn_on();
+    else {
+        output.turn_off();
+    }
+}
---
+++ Core/Src/HVSCU/HVSCU.cpp
@@ -0,0 +1,27 @@
+#include "HVSCU/HVSCU.hpp"
+
+HVSCU::HVSCU()
+    : low(DigitalOutput(PG14), true),
+      high(DigitalOutput(PG12), true),
+      precharge(DigitalOutput(PD4), true),
+      discharge(DigitalOutput(PF4), false) {}
+
+void HVSCU::open_contactors() {
+    if (timeout_id) Time::cancel_timeout(timeout_id);
+    discharge.close();
+    low.open();
+    high.open();
+    precharge.open();
+}
+
+void HVSCU::close_contactors() {
+    if (timeout_id) Time::cancel_timeout(timeout_id);
+    discharge.open();
+    low.close();
+    precharge.close();
+    high.open();
+    timeout_id = Time::set_timeout(3000, [this]() {
+        precharge.open();
+        high.close();
+    });
+}
